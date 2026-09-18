a4344d2aef02dd37b64ecfa2dcec06f5dfff741b
pereira.1959 <lfapereira6@gmail.com>
2026-09-17 22:13:42 -0300

    Remove script auxiliar deixado por engano

--- git-commit.ps1
+++
@@ -1,6 +0,0 @@
-cd "C:\Users\pereira.1959\Documents\mini-app-pokeapi"
-git add -A
-git status
-git commit -m "Adiciona sons dos Pokemon, lendarios extras e fundo de pokebolas"
-git push
-Remove-Item "git-commit.ps1" -Force
