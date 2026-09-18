c6d3fd591aea921cb26a89fe8ee5223cef1ba67b
pereira.1959 <lfapereira6@gmail.com>
2026-09-17 22:07:41 -0300

    Remove script auxiliar deixado por engano

--- git-commit.ps1
+++
@@ -1,5 +0,0 @@
-cd "C:\Users\pereira.1959\Documents\mini-app-pokeapi"
-git add -A
-git status
-git commit -m "Adiciona validacao de entrada, tema visual estilo Pokedex e fotos de Pokemon para os IDs 1-10"
-git push
